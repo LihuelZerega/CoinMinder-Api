cbdb2aa2f490b620c460466bd14b3fd410794f85
LihuelZerega <zeregalihuel@gmail.com>
2024-02-09 23:35:18 -0300

    Updates update

--- src/services/CryptosUpdateDatas/updateBITCOINCASHData.js
+++ src/services/CryptosUpdateDatas/updateBITCOINCASHData.js
@@ -1,17 +1,17 @@
 const axios = require('axios');
 require("dotenv").config();
-const { AVALANCHEcrypto } = require('../../db');
+const { BITCOINCASHcrypto } = require('../../db');
 
 async function fetchDataAndStoreInDatabase() {
   try {
-    const response = await axios.get(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=avalanche-2&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en`);
+    const response = await axios.get(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=bitcoin-cash&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en`);
     
     if (response.data && response.data.length > 0) {
-      const avalancheData = response.data[0];
+      const bitcoincashData = response.data[0];
 
       const { symbol, name, image, current_price, market_cap, market_cap_rank, fully_diluted_valuation, total_volume, high_24h, low_24h, price_change_24h, price_change_percentage_24h, market_cap_change_24h, market_cap_change_percentage_24h, circulating_supply, total_supply, max_supply, ath, ath_change_percentage, ath_date, roi, last_updated } = bitcoinData;
 
-      await AVALANCHEcrypto.upsert({
+      await BITCOINCASHcrypto.upsert({
         symbol,
         name,
         image,
@@ -36,12 +36,12 @@ async function fetchDataAndStoreInDatabase() {
         last_updated
       });
 
-      console.log('Datos de AVALANCHE actualizados correctamente.');
+      console.log('Datos de BITCOIN CASH actualizados correctamente.');
     } else {
-      console.error('La respuesta de la API de CoinGecko está vacía o no contiene datos para AVALANCHE.');
+      console.error('La respuesta de la API de CoinGecko está vacía o no contiene datos para Bitcoin Cash.');
     }
   } catch (error) {
-    console.error('Error al actualizar los datos de AVALANCHE:', error.message);
+    console.error('Error al actualizar los datos de BITCOIN CASH:', error.message);
     throw error;
   }
 }
